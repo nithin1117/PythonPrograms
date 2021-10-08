bd80d05b0fc1c7e4e45e923c9e63173fd22ad28a
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-10-08 15:23:23 +0530

    Create Instagram_dp.py

---
+++ Instagram_dp.py
@@ -0,0 +1,9 @@
+import instaloader
+ig = instaloader.Instaloader()
+dp = input("enter: ")
+ig.download_profile(dp, profile_pic_only = True)
+
+
+
+# pip3 install instaloader
+# instaloader profile [profile ...]
